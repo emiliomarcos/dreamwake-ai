10ec42539a58e440d7b4f2f36bee413308146e1f
Emilio Marcos <emilio7marcos@gmail.com>
2023-06-16 22:10:37 -1000

    basic lucid dreaming info

--- client/src/components/Lucid/Lucid.jsx
+++ client/src/components/Lucid/Lucid.jsx
@@ -33,6 +33,32 @@ export default function Lucid() {
   }
 
   return (
-    loadingStatus ? <Loader /> : lucidOutput ? lucidOutput : <button onClick={handleClick}>Ask AI</button>
+    <>
+      <div className="lucid-info">
+        <h2>What is Lucid Dreaming?</h2>
+        <p>Lucid dreaming is the phenomenon where a dreamer becomes aware that they are dreaming, while they are in the dream. This awareness can range from faint recognition of the dream state to a momentous broadening of perspective. Lucid dreamers can often manipulate and control their dreams, creating an extraordinary virtual reality.</p>
+
+        <h2>Techniques to Induce Lucid Dreaming</h2>
+        <ul>
+          <li><strong>Mnemonic Induction of Lucid Dreams (MILD)</strong>: This technique involves waking up after a period of sleep and then developing the intention to remember that you are dreaming before returning to sleep.</li>
+          <li><strong>Wake Back to Bed (WBTB)</strong>: WBTB involves waking up for a few minutes after a period of sleep and then going back to sleep, aiming to enter a REM sleep period where dreams are more likely to occur.</li>
+          <li><strong>Wake Induced Lucid Dreams (WILD)</strong>: This technique is based on transitioning from the awake state directly into the dream state while maintaining consciousness.</li>
+        </ul>
+
+        <h2>Benefits of Lucid Dreaming</h2>
+        <p>Lucid dreaming provides a uniquely rich and vivid dream world, where everything you see, hear, touch, taste, and smell can be as authentic as reality. Some of the key benefits include:</p>
+        <ul>
+          <li>Enhancing creativity and problem-solving skills</li>
+          <li>Practicing real-life skills</li>
+          <li>Overcoming nightmares</li>
+          <li>Exploring personal issues and self-reflection</li>
+        </ul>
+      </div>
+      {loadingStatus ? <Loader /> : lucidOutput ? lucidOutput : <button onClick={handleClick}>Ask AI</button>}
+  </>
   )
+
+  // return (
+  //   loadingStatus ? <Loader /> : lucidOutput ? lucidOutput : <button onClick={handleClick}>Ask AI</button>
+  // )
 }
